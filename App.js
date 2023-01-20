4aa389d6d03c0c2a2f8ada1be3d00df0f025d2fa
Guilherme Santana <g.santossantana22@gmail.com>
2023-01-20 09:51:33 -0300

    atualização

--- App.js
+++ App.js
@@ -1,32 +1,35 @@
-import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
 import { Button, Image, View } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 
 export default function ImagePickerExample() {
-  const [image, setImage] = useState(null);
+  const [status, requestPermission] = ImagePicker.useCameraPermissions();
+  useEffect(() => {
+    async function verPermissoes() {
+      const cameraStatus = await ImagePicker.requestCameraPermissionsAsync();
+      requestPermission(cameraStatus === "granted");
+    }
 
-  const pickImage = async () => {
-    // No permissions request is necessary for launching the image library
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
+    verPermissoes();
+  });
+
+  const acessaCamera = async () => {
+    const imagem = await ImagePicker.launchCameraAsync({
       allowsEditing: true,
-      aspect: [9, 16],
+      aspect: [16, 9],
       quality: 0.5,
     });
 
-    console.log(result);
-
-    if (!result.canceled) {
-      setImage(result.assets[0].uri);
-    }
+    console.log(imagem);
   };
 
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Button title="Pick an image from camera roll" onPress={pickImage} />
-      {image && (
-        <Image source={{ uri: image }} style={{ width: 300, height: 400 }} />
-      )}
-    </View>
+    <>
+      <StatusBar />
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Button title="acessar câmera" onPress={acessaCamera} />
+      </View>
+    </>
   );
 }
